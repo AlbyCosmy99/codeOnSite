ed3690db7ac347741b80d5d3149c5c88fee63010
Andrei <dinamo1999@icloud.com>
2024-07-27 23:49:50 +0200

    changes

--- xmlhttprequest.js
+++ xmlhttprequest.js
@@ -59,6 +59,8 @@ export function changeComponent(page, fromEditor=false, pageToEditor=null, isEdi
     xmlhttp.open('GET', BACKEND + 'api/pages/' + page, true)
     xmlhttp.setRequestHeader('Content-Type', 'text/html');
     xmlhttp.send()
+
+    sessionStorage.setItem('currentPage', page)
     
     if(!isEdit) {
         getNavbarOptions()
