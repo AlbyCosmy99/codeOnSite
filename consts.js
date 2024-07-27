0361e0dddb5d6cad0823d72ac9ec5cf05b24aa78
Andrei <dinamo1999@icloud.com>
2024-07-27 22:28:31 +0200

    changes

--- consts.js
+++ consts.js
@@ -1,6 +1,7 @@
-export const BACKEND_DEV = 'lhttp://localhost:3000/'
-export const BACKEND_PROD = 'https://frontend-code-on-site-backend.onrender.com/'
+export const BACKEND_DEV = 'http://localhost:3000/';
+export const BACKEND_PROD = 'https://frontend-code-on-site-backend.onrender.com/';
 
-export const DEBUG = false
+export const DEBUG = false;
 
-export default BACKEND = DEBUG ? BACKEND_DEV : BACKEND_PROD
+const BACKEND = DEBUG ? BACKEND_DEV : BACKEND_PROD;
+export default BACKEND;
